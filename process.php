1f4f30b4e673c9988fd0fd0fe869ed461668dde3
Ignacio Zuñiga <ignaciozuniga22@gmail.com>
2019-06-04 20:10:59 -0400

    1.1

---
+++ process.php
@@ -0,0 +1,36 @@
+<?php
+	if (isset($_POST['fname'])){
+		$nombres=htmlentities($_POST['fname']);
+		$email_cliente=htmlentities($_POST['email']);
+		$telefono=htmlentities($_POST['phone']);
+		$subject=utf8_decode($_POST['subject']);
+		$mensaje=htmlentities($_POST['message']);
+
+		
+	/*SIGUE RECOLECTANDO DATOS PARA FUNCION MAIL*/
+	$message = '';
+	$message .= '<p>Hola, tiene un nuevo mensaje desde el sitio digital5.cl:</p> ';
+	$message .= '<p>Remitente: '.$nombres.'</p> ';
+	$message .= '<p>Email: '.$email_cliente.'</p> ';
+	$message .= '<p>Teléfono: '.$telefono.'</p> ';
+	$message .= '<p>Mensaje: '.$mensaje.'</p> ';
+	
+	
+
+	
+	$header = "MIME-Version: 1.0\r\n";
+	$header .= "Content-type: text/html; charset=UTF-8\r\n";
+	$header .= "From: ". $nombres . " <" . $email_cliente . ">\r\n";
+	$email='skainet-star@hotmail.cl';//Ingresa tu dirección de correo
+	
+			
+	if (mail($email,$subject,$message,$header)){
+		echo 'success';
+	}	 else {
+		echo 'No se pudo enviar el mensaje.';
+	}
+	/*FINALIZA RECOLECTANDO DATOS PARA FUNCION MAIL*/
+	
+	
+	}
+?>
